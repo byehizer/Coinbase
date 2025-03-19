5e5623e05c818d90a0077501141e878604172cdd
FrancoJarc <francoj1612@gmail.com>
2025-03-18 21:18:45 -0300

    Segunda fusion. Un error modificado, codigo listo para usar

--- coinbasefront/src/App.jsx
+++ coinbasefront/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import { Navbar } from './components/Navbar';
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { appRoutesAdmin, appRoutesAdminAddProduct } from './routes/routes';
 import { ModalProvider } from './context/ModalContext';
 import { AiOutlineSearch } from "react-icons/ai";
 import { SearchBar } from './components/Navbar/components/Searchbar';
 import { ShoppingCartProvider } from './context/shoppingcartcontext';
+import { Navbar } from './components/Navbar/Navbar';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,16 +17,17 @@ function App() {
     <>
       <BrowserRouter>
         <ModalProvider>
+          <ShoppingCartProvider>
           <Navbar></Navbar>
-          <ShoppingCartProvider></ShoppingCartProvider>
-          <Routes>
-            {appRoutesAdmin.map(route => (
-              <Route key={route.name} path={route.path} element={route.element} />
-            ))}
-            {appRoutesAdminAddProduct.map(route => (
-              <Route key={route.name} path={route.path} element={route.element} />
-            ))}
-          </Routes>
+            <Routes>
+              {appRoutesAdmin.map(route => (
+                <Route key={route.name} path={route.path} element={route.element} />
+              ))}
+              {appRoutesAdminAddProduct.map(route => (
+                <Route key={route.name} path={route.path} element={route.element} />
+              ))}
+              </Routes>
+          </ShoppingCartProvider>
         </ModalProvider>
       </BrowserRouter>
 
